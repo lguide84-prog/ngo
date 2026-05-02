ca3b560614eb64a0a9041813c1653b73e064439b
deepak445566 <deepakpandey1212d@gmail.com>
2026-05-02 14:32:43 +0530

    hgfhgfh

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ await connectCloudinary();
 // Simple CORS configuration
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ngo-phi-hazel.vercel.app",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
